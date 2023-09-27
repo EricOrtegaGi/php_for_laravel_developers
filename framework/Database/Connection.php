7159c4bf24b58bedaa6ac2c2ae0807c4e6de7b68
EricOrtegaGi <ericortega@iesebre.com>
2023-09-27 17:02:57 +0200

    103 funcionant

--- framework/Database/Connection.php
+++ framework/Database/Connection.php
@@ -1,25 +1,8 @@
 <?php
+
 namespace framework\Database;
 
-use PDO;
-
-class Conection
+class Connection
 {
-    private $config;
 
-    public function __construct($config)
-    {
-        $this->config = $config;
-    }
-
-    function connectDB(){ //Dependency Injection
-        try {
-            return new PDO(
-                $this->config['database']['databasetype'] . ':host=' . $this->config['database']['host'] . ';dbname=' . $this->config['database']['name'],
-                $this->config['database']['user'],
-                $this->config['database']['password']);
-        }catch (\Exception $e){
-            echo 'Error de connexio en la base de dades';
-        }
-    }
 }
